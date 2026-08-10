1466dd7c885cac5e8aeb082878dc09e8fc34829c
Jake Hathaway <jacob.d.hathaway2.civ@us.navy.mil>
2026-08-10 13:51:00 -0700

    Use old application migration structure so old passwords can be migrated to new app seamlessly.

---
+++ CrossPlatformPasswordManager.Core/Migrations/20251011003933_CreateMasterPasswordTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrossPlatformPasswordManager.Core.Migrations;
+
+/// <inheritdoc />
+public partial class CreateMasterPasswordTable : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        _ = migrationBuilder.CreateTable(
+            name: "MasterPasswords",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "INTEGER", nullable: false)
+                    .Annotation("Sqlite:Autoincrement", true),
+                PasswordHash = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                KeyDerivationSalt = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false)
+            },
+            constraints: table =>
+            {
+                _ = table.PrimaryKey("PK_MasterPasswords", x => x.Id);
+            });
+
+        _ = migrationBuilder.Sql(@"
+insert into MasterPasswords (Id, PasswordHash, KeyDerivationSalt) values
+(1, '', '');
+");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) => migrationBuilder.DropTable(
+            name: "MasterPasswords");
+}
---
+++ CrossPlatformPasswordManager.Core/Migrations/20251011185039_CreatePasswordEntriesTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrossPlatformPasswordManager.Core.Migrations;
+
+/// <inheritdoc />
+public partial class CreatePasswordEntriesTable : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.CreateTable(
+            name: "PasswordEntries",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "INTEGER", nullable: false)
+                    .Annotation("Sqlite:Autoincrement", true),
+                SiteName = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                Username = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                PasswordHash = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false)
+            },
+            constraints: table =>
+            {
+                _ = table.PrimaryKey("PK_PasswordEntries", x => x.Id);
+            });
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) => migrationBuilder.DropTable(
+            name: "PasswordEntries");
+}
